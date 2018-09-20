727326bb85e2a2e0cd07d3d16f2b8d5a173dd17d
Rolly Padua <32024336+Vandolph703@users.noreply.github.com>
2018-09-20 08:58:05 -0700

    Add files via upload

---
+++ Hero.cpp
@@ -0,0 +1,32 @@
+#define _USE_MATH_DEFINES
+#include <math.h>
+#include "Hero.h"
+
+/*
+void Hero::draw(){
+    //glColor3f(r, g, b);
+    
+    glBegin(GL_POLYGON);
+    
+	glVertex2f(x, y);
+	glVertex2f(x + width, y);
+	glVertex2f(x + width, y - height);
+	glVertex2f(x, y - height);
+    
+    glEnd();
+}
+
+void dead(){			//If player dead, game over	//void or bool?
+	//player is dead
+}
+void placeBomb() {		//basics
+	//put bomb and start timer 
+}
+void kickBomb() {		//Power up, moves bomb
+	/*
+	if contains = true
+		move bomb in certain direction
+	
+	*/
+}
+*/
